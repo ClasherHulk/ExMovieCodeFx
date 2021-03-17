52c1ca3f61bd61571bb080f32dbb0ae5c8de05cd
ClasherHulk <krnskrwr4@gmail.com>
2021-03-17 16:04:46 +0530

    commit

---
+++ Data/MovieDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ExCoreWebAPICODEFx.Models;
+
+namespace ExCoreWebAPICODEFx.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext (DbContextOptions<MovieDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ExCoreWebAPICODEFx.Models.Movie> Movie { get; set; }
+    }
+}
